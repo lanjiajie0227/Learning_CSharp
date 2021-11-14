dc7b88b5356e535022658a7d4d179957d2f3ffd2
家杰 蓝 <lanjiajie5@live.com>
2021-11-15 00:07:47 +0800

    第六次修改

---
+++ CSharp/LeetCode算法/ChangeIntToRoman.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp.CSharp.LeetCode算法
+{
+    class ChangeIntToRoman
+    {
+        public string IntToRoman(int num)
+        {
+            if (num < 1 || num > 3999)
+            {
+                return null;
+            }
+            //将几个特殊的情况的数字全部列举出来
+            int[] nums = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+            string[] romans = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+            int index = 0;
+            StringBuilder res = new StringBuilder();
+            while (index < 13)
+            {
+                while (num >= nums[index])  //对比情况
+                {
+                    res.Append(romans[index]);
+                    num -= nums[index];
+                }
+                index++;
+            }
+            return res.ToString();
+        }
+    }
+}
---
+++ CSharp/LeetCode算法/MaxCount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp.CSharp.LeetCode算法
+{
+    class Max_Count
+    {
+        // 不用去依次相加 再计数
+        // 观察可知，最大的数必然是操作次数最多的数，找共同被ops操作到的次数就行了。即找ops数组中每一列最小值，然后相乘。
+        //找出每一次都被操作到的区域的面积就可以了
+
+        public int MaxCount(int m, int n, int[][] ops)
+        {
+            int mina = m, minb = n;
+            foreach (int[] op in ops)
+            {
+                mina = Math.Min(mina, op[0]);
+                minb = Math.Min(minb, op[1]);
+            }
+            return mina * minb;
+        }
+    }
+}
